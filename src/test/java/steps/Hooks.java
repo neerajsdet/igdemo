2e11de7534a7c7947990936483374134a41a4969
vinod29kumar <vinod29.kumar@paytm.com>
2024-02-05 14:02:04 +0530

    environment support added

--- src/test/java/steps/Hooks.java
+++ src/test/java/steps/Hooks.java
@@ -3,25 +3,28 @@ package steps;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeAll;
 import io.cucumber.java.Scenario;
+import lombok.extern.slf4j.Slf4j;
 import org.paytm.insurance.reports.ScenarioUtils;
 import org.paytm.insurance.utils.PropertiesHelper;
 
-
+@Slf4j
 public class Hooks {
 
 
-  @BeforeAll
-  public static void runBeforeAllScenarios() {
-    String config = "src/test/resources/config/staging/config.properties";
-    String endpoints = "src/test/resources/endpoints/endpoint.properties";
-    PropertiesHelper.loadProperties(config, endpoints);
-  }
+    @BeforeAll
+    public static void runBeforeAllScenarios() {
+        String env = System.getProperty("env") != null ? System.getProperty("env") : "staging";
+        log.info("Running on environment: {}", env);
+        String config = "src/test/resources/profiles/" + env + "/config.properties";
+        String endpoints = "src/test/resources/endpoints/endpoint.properties";
+        PropertiesHelper.loadProperties(config, endpoints);
+    }
 
 
-  @Before(order = 1)
-  public void runBeforeEachScenario(final Scenario scenario) {
-    ScenarioUtils.putScenario(scenario);
-  }
+    @Before(order = 1)
+    public void runBeforeEachScenario(final Scenario scenario) {
+        ScenarioUtils.putScenario(scenario);
+    }
 
 
 }
